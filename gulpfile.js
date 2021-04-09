eb515df836bb56ea9956feda1f4894f98ed543fe
dxl <1799097002@qq.com>
2021-04-09 16:36:36 +0800

    shouye

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,34 @@ const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 const rename = require("gulp-rename");
 const babel = require("gulp-babel");
+const sourcemaps = require("gulp-sourcemaps");
 
 gulp.task("html", done => {
-    gulp.src("*.html").pipe(gulp.dest("dist/html")).pipe(connect.reload());
+    gulp.src("*.html!index.html").pipe(gulp.dest("dist/html")).pipe(connect.reload());
     done();
 })
+gulp.task("indexhtml", done => {
+    gulp.src("index.html").pipe(gulp.dest("dist")).pipe(connect.reload());
+    done();
+})
+
 gulp.task("css", done => {
     gulp.src("sass/*.scss")
-        .pipe(sass()).pipe(gulp.dest("dist/css"))
+        .pipe(sourcemaps.init())
+        // .pipe(sass({ outputStyle: "compressed" }))
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("dist/css"))
         .pipe(connect.reload());
     done();
 });
-
+// img
+gulp.task("img", done => {
+    gulp.src("img/**")
+        .pipe(gulp.dest("dist/img"))
+        .pipe(connect.reload());;
+    done();
+});
 // 将js文件进行压缩，生成压缩文件和拷贝原文件
 gulp.task("js", done => {
     gulp.src("js/*.js")
@@ -45,10 +61,11 @@ gulp.task("babel", done => {
 
 //监听文件变化，一旦监听的文件发生变化，就执行指定的任务
 gulp.task("watch", done => {
-
+    gulp.watch("index.html", gulp.series("indexhtml"));
     gulp.watch("*.html", gulp.series("html"));
     gulp.watch("sass/*.scss", gulp.series("css"));
     gulp.watch("js/*.js", gulp.series("js"));
+    gulp.watch("img/**", gulp.series("img"));
 
     done();
 })
@@ -62,5 +79,5 @@ gulp.task("server", done => {
 
     done();
 });
-gulp.task("build", gulp.series("html", "css", "js"));
+gulp.task("build", gulp.series("indexhtml", "html", "css", "js", "img", "babel"));
 gulp.task("default", gulp.series("build", "server", "watch"));
